02c8b81b58d1b9455bbd55c4be85f4be7458e0c9
近藤颯人 <hayatokondo@kondousatsuhitonoMacBook-Air.local>
2020-07-25 15:17:56 +0900

    Group Function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,41 @@
 class GroupsController < ApplicationController
 
+  def index
+  end
+
   def new
+    @group = Group.new
+    @group.users << current_user
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+      # redirect_to はルーティング，コントローラを介してビューの表示
+    else
+      render :new
+      # render はインスタンス変数を上書きせずにビューをそのまま表示
+    end
   end
-  
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループを更新しました'
+    else
+      render :edit
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [])
+    # 配列の保存は(キー： [])
+  end
+
 end
